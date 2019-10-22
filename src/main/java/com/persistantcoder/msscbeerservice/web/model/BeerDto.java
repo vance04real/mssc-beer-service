c16bb0dce8a7c75e8bab0ba95d217f0377523b0b
Evans K F Chikuni <vance04real@gmail.com>
2019-10-22 09:53:27 +0200

    add web data model. closes #2

---
+++ src/main/java/com/persistantcoder/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,36 @@
+package com.persistantcoder.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by Evans K F C on Oct ,2019
+ **/
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyleEnum;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/com/persistantcoder/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package com.persistantcoder.msscbeerservice.web.model;
+
+/**
+ * Created by Evans K F C on Oct ,2019
+ **/
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT,GOSE ,PORTER , ALE ,WHEAT, IPA, PALE_ALE, SAISON;
+}
